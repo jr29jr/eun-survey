7896d488b577f4382f060d57cc855611aac5b28b
jr29jr <jr29jr@gmail.com>
2023-01-20 13:25:47 +0900

    user answer service update

--- src/user_answer/user_answer.service.ts
+++ src/user_answer/user_answer.service.ts
@@ -1,26 +1,41 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateUserAnswerInput } from './dto/create-user_answer.input';
 import { UpdateUserAnswerInput } from './dto/update-user_answer.input';
+import { UserAnswer } from './entities/user_answer.entity';
 
 @Injectable()
 export class UserAnswerService {
-  create(createUserAnswerInput: CreateUserAnswerInput) {
-    return 'This action adds a new userAnswer';
+  constructor(
+    @InjectRepository(UserAnswer)
+    private userAnswerRepository: Repository<UserAnswer>,
+  ) {}
+
+  async create(createUserAnswerInput: CreateUserAnswerInput) {
+    return await this.userAnswerRepository.save(createUserAnswerInput);
   }
 
-  findAll() {
-    return `This action returns all userAnswer`;
+  async findOne(id: number) {
+    const result=await this.userAnswerRepository.findOneBy({id});
+    //없는 id접근하는 경우 처리하자
+
+    return result;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} userAnswer`;
+  async update(id: number, updateUserAnswerInput: UpdateUserAnswerInput) {
+    const result=await this.userAnswerRepository.update(id,updateUserAnswerInput);
+    if(result.affected === 0)
+        throw new NotFoundException();
+    return this.userAnswerRepository.findOneBy({id});
   }
 
-  update(id: number, updateUserAnswerInput: UpdateUserAnswerInput) {
-    return `This action updates a #${id} userAnswer`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} userAnswer`;
+  async remove(id: number) {
+    //정상적인 상황에서도 null 리턴하는거 처리해야한다.
+    const result = await this.userAnswerRepository.delete(id);
+    console.log(result);
+    if(result.affected === 0)
+        throw new NotFoundException();
+    return null;
   }
 }
